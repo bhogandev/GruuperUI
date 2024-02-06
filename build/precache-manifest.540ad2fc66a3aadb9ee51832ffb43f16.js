083026f28a2e671f3730f3e10d81af5398402256
Brandon Hogan <bhogandev@outlook.com>
2024-02-06 01:14:45 -0500

    deployment update heroku

---
+++ build/precache-manifest.540ad2fc66a3aadb9ee51832ffb43f16.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02a6217f977b253d2de9bff3bdf4d6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eb7def5a866a74003f6",
+    "url": "/static/css/2.72801d0f.chunk.css"
+  },
+  {
+    "revision": "bf79b4d517cbe7736b67",
+    "url": "/static/css/main.d50eb1e1.chunk.css"
+  },
+  {
+    "revision": "7eb7def5a866a74003f6",
+    "url": "/static/js/2.81e3c02e.chunk.js"
+  },
+  {
+    "revision": "c63385931482eb7db09c69549ebcf75f",
+    "url": "/static/js/2.81e3c02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf79b4d517cbe7736b67",
+    "url": "/static/js/main.69cf52d7.chunk.js"
+  },
+  {
+    "revision": "30bbcf792e003e6932e42548f61f64fc",
+    "url": "/static/js/main.69cf52d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f3b3ec621f410920621",
+    "url": "/static/js/runtime-main.176ce1a3.js"
+  },
+  {
+    "revision": "4398eb545b745dbb5052a1751e09ad23",
+    "url": "/static/media/GruuperFull.4398eb54.png"
+  },
+  {
+    "revision": "b4bff2f4e0d8c04c2a767b458865f00d",
+    "url": "/static/media/lanphoman.b4bff2f4.jpg"
+  }
+]);
